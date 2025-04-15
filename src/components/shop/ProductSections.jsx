13eb5c0559a6ccfe51f981720bde2bb1043454f0
Gouthami200119 <gouthami@unipolar.in>
2025-04-15 10:53:42 +0530

    header

--- src/components/shop/ProductSections.jsx
+++ src/components/shop/ProductSections.jsx
@@ -4,7 +4,6 @@ const ProductSections = () => {
   return (
     <div className=" mx-auto  px-4 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 h-[400px]">
-        {/* Best Sellers Section */}
         <div className="relative bg-[#A0A0A0]  flex items-center justify-center text-white text-center  overflow-hidden">
           <div className="z-10">
             <h2 className="text-3xl md:text-4xl font-bold mb-2">Stock Products</h2>
@@ -12,11 +11,10 @@ const ProductSections = () => {
           </div>
         </div>
 
-        {/* New Arrivals Section */}
         <div className="relative bg-[#A0A0A0] flex items-center justify-center text-white text-center  overflow-hidden">
           <div className="z-10">
             <h2 className="text-3xl md:text-4xl font-bold mb-2">Customization Tool</h2>
-            <a href="#" className="hover:underline text-sm md:text-base">Click here</a>
+            <a href="/configuration-tool" className="hover:underline text-sm md:text-base">Click here</a>
           </div>
         </div>
       </div>

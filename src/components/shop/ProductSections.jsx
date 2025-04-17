b9092d4625de5270578517ab158efa96e4a35eff
Gouthami200119 <gouthami@unipolar.in>
2025-04-17 10:57:56 +0530

    images

--- src/components/shop/ProductSections.jsx
+++ src/components/shop/ProductSections.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import ProductStock from '../../../public/home/bgimg2.png';
+import Customization from '../../../public/Pictures/Creator.png';
 
 const ProductSections = () => {
   return (
-    <div className=" mx-auto  px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 h-[400px]">
-        <div className="relative bg-[#A0A0A0]  flex items-center justify-center text-white text-center  overflow-hidden">
-          <div className="z-10">
+    <div className="mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 h-[450px]">
+        <div className="relative flex items-center justify-center text-white text-center overflow-hidden">
+          <img src={ProductStock.src} alt="Stock Products" className="absolute inset-0 w-full h-full object-cover z-0" />
+          <div className="z-10  p-4">
             <h2 className="text-3xl md:text-4xl font-bold mb-2">Stock Products</h2>
             <a href="/all-category" className="hover:underline text-sm md:text-base">Click here</a>
           </div>
         </div>
 
-        <div className="relative bg-[#A0A0A0] flex items-center justify-center text-white text-center  overflow-hidden">
-          <div className="z-10">
+        <div className="relative flex items-center justify-center text-white text-center overflow-hidden">
+          <img src={Customization.src} alt="Customization Tool" className="absolute inset-0 w-full h-full object-cover z-0" />
+          <div className="z-10  p-4">
             <h2 className="text-3xl md:text-4xl font-bold mb-2">Customization Tool</h2>
             <a href="/configuration-tool" className="hover:underline text-sm md:text-base">Click here</a>
           </div>

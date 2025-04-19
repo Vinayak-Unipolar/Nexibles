90ab6fd0b7c809b2d984398aef26f11cb051563a
Siddhant <siddhant999123@gmail.com>
2025-04-19 11:35:38 +0530

    hello

--- src/components/StatsAndTestimonials/StatsAndTestimonials.jsx
+++ src/components/StatsAndTestimonials/StatsAndTestimonials.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import client1 from '../../../public/client/client1.png';

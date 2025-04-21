bc13dc7be0b62d58ae6f607b1793d336ee4d9d13
Gouthami192001 <manchegouthami@gmail.com>
2025-04-21 16:48:49 +0530

    m

--- src/components/shop/ProductSections.jsx
+++ src/components/shop/ProductSections.jsx
@@ -12,23 +12,23 @@ const fadeSlide = {
 const ProductSections = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 h-[500px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:h-[500px]">
 
         {/* 1 */}
         <Link href="/all-category" className="relative overflow-hidden group">
           <img
             src={ProductStock.src}
             alt="Stock Products"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-contain"
           />
           <motion.div
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
             variants={fadeSlide}
-            className="absolute inset-0 bg-black bg-opacity-0 flex flex-col justify-end items-end pr-6 md:pr-14 pb-10 text-white space-y-2 text-right"
+            className="absolute inset-0 bg-black bg-opacity-0 flex flex-col text-white space-y-2 top-[52%] left-[49%]"
           >
-            <h2 className="text-2xl md:text-4xl font-semibold border-b border-white inline-block pr-2">
+            <h2 className="text-2xl md:text-4xl font-semibold inline-block pr-2">
               EXPLORE
             </h2>
             <h1 className="text-3xl md:text-5xl font-bold">NEXICLASSIC</h1>
@@ -40,16 +40,16 @@ const ProductSections = () => {
           <img
             src={Customization.src}
             alt="Customization Tool"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-contain"
           />
           <motion.div
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
             variants={fadeSlide}
-            className="absolute inset-0 bg-black bg-opacity-0 flex flex-col items-start pl-6 md:pl-14 pt-14 text-white space-y-2"
+            className="absolute inset-0 bg-black bg-opacity-0 flex flex-col top-[30%] right-[35%] left-[7%] text-white space-y-2"
           >
-            <h2 className="text-2xl md:text-4xl font-semibold border-b border-white inline-block pr-4">
+            <h2 className="text-2xl md:text-4xl font-semibold inline-block pr-4">
               MAKE YOUR OWN
             </h2>
             <h1 className="text-3xl md:text-5xl font-bold">POUCH</h1>

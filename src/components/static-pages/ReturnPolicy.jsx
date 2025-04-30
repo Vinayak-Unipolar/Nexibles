d8db782615d035d300eab96367b3d32e922ce3a9
nd07-02 <desainikita2002@gmail.com>
2025-04-30 14:54:56 +0530

    push shipping

--- src/components/static-pages/ReturnPolicy.jsx
+++ src/components/static-pages/ReturnPolicy.jsx
@@ -1,74 +1,42 @@
-import React from 'react'
-
+import React from 'react';
 
 const ReturnPolicy = () => {
     return (
         <div className="h-auto bg-white p-16">
-            <h2 className="text-gray-900 text-3xl font-semibold text-center mb-8">Return & Refund Policy for nexibles.com</h2>
+            <h2 className="text-gray-900 text-3xl font-semibold text-center mb-8">Return & Refund Policy </h2>
             <div>
-                <div className="text-gray-900 mb-2">
-                    <p>
-                        We strive to ensure that you have a satisfying shopping experience with us. Our Return Policy outlines guidelines for returns, applicable only in the case of damaged or wrong products.
-                    </p>
-                </div>
                 <div className="text-gray-900 text-base">
-                    <h4 className="font-bold mb-1">1. Eligibility for Returns:</h4>
+                    <h4 className="font-bold mb-1">Refund Process:</h4>
                     <p className='mb-1'>
-                        You are eligible to request a return only in the following situations:
-                    </p>
-
-                    <p className='mb-1'>
-                        <span className="font-bold text-lg">Damaged Product: </span>
-                        If the product you received is damaged during transit or has a manufacturing defect.
+                        Refund, if any, shall be made at the same issuing bank through which the Product/Service was purchased. For cash on delivery transactions, the Member has the option to receive the refund in any bank account via NEFT and it is the responsibility of the Member to share the valid bank details to avail the said return. Refund shall be made in Indian Rupees only and shall be equivalent to the Transaction Price received in Indian Rupees.
                     </p>
                     <p className='mb-1'>
-                        <span className="font-bold text-lg">Wrong Product: </span>
-                        If you receive a product different from the one you ordered.
+                        For payments made through electronic means like debit card, credit card, net banking, wallet etc. refund shall be made using the same payment mode or any other online banking / electronic funds transfer system approved by Reserve Bank India (RBI). Refunds may be supported for select banks. Where a bank is not supported for processing refunds, you will be required to share alternate bank account details with us for processing the refund. Refund shall be conditional and shall be with recourse available to VistaPrint in case of any misuse by you. We may also request you for additional documents for verification.
                     </p>
-                    <h4 className="font-bold">2. Return Process:</h4>
-                    <p className='mb-1'>To initiate a return, please follow these steps:</p>
-                    <p className='mb-1'>- Contact our Customer Support team at <a className="text-red-500" href="mailto:support@nexibles.com">support@nexibles.com</a> within 7 days of receiving the product.</p>
-                    <p className='mb-1'>- Provide your order number, details of the damaged or wrong product, and photographic evidence if applicable.</p>
-                    <p className='mb-1'>- Our Customer Support team will guide you through the return process and provide instructions on returning the product.</p>
-                    <h4 className="font-bold">3. Conditions for Return</h4>
-                    <p className='mb-1'>The product must be in its original packaging, unused, and in the same condition as when you received it.</p>
+                    <h4 className="font-bold mb-1">Transaction Restrictions:</h4>
                     <p className='mb-1'>
-                        All tags, labels, and accessories must be intact.
+                        VistaPrint reserves the right to refuse to process transactions from you if you have a prior history of questionable charges including without limitation breach of any agreements by you with VistaPrint or breach/violation of any law or any charges imposed by Issuing Bank or breach of any policy.
                     </p>
-                    <h4 className="font-bold">4. Inspection and Approval</h4>
-                    <p className='mb-1'>Once we receive your returned product, we will inspect it to verify if it meets the conditions for return. If approved, we will process the refund or replacement.</p>
-                    <p className='mb-1'>The refund will be issued using the original payment method, unless agreed upon otherwise.</p>
-                    <h4 className="font-bold">5. Refund or Replacement</h4>
-                    <p className='mb-1'>In the case of an approved return:</p>
+                    <h4 className="font-bold mb-1">Refund Verification:</h4>
                     <p className='mb-1'>
-                        <span className="font-bold text-lg"> Refund :</span> We will process a refund to your original payment method within 7 days.
+                        On request from You, VistaPrint will provide ARN (Acquirer Reference Number) or UTR (Unique Transaction Reference Number) to you for refunds made to you. However, in an event of non-receipt of such a refund in the designated Bank Account it will be your responsibility to provide VistaPrint with the set of documents required to support the claim of non-receipt.
                     </p>
                     <p className='mb-1'>
-                        <span className="font-bold text-lg"> Replacement:</span> If you choose a replacement, we will dispatch the correct product to you at the earliest.
+                        VistaPrint through its payments service provider shall initiate the refund procedure subject to satisfactory verification of the documents provided by the member.
                     </p>
-                    <h4 className="font-bold">6. Non-Returnable Items: </h4>
+                    <h4 className="font-bold mb-1">Merchant Credit Option:</h4>
                     <p className='mb-1'>
-                        Certain products may not be eligible for return. These include:
+                        If you chose to avail Merchant Credit in lieu of refund, such credit will be available for use by you for a period of (45) forty-five days. Post such period, the Merchant Credit shall lapse and would not be usable for any future payments by you.
                     </p>
+                    <h4 className="font-bold mb-1">Contact Information:</h4>
                     <p className='mb-1'>
-                        Products without original packaging or tags.
+                        If you have any questions or concerns about our Refund Policy, please contact our Customer Support team at <a className="text-red-500" href="mailto:support@nexibles.com">support@nexibles.com</a>
                     </p>
-                    <p className='mb-1'>
-                        Products that show signs of use or damage not caused during transit.
-                    </p>
-                    <h4 className="font-bold">7. Contact Information</h4>
-                    <p className='mb-1'>If you have any questions or concerns about our Return Policy, please contact our Customer Support team at <a href="mailto:support@nexibles.com">support@nexibles.com</a></p>
-                    <h4 className="font-bold">8. Changes to this Return Policy:</h4>
-                    <p className='mb-1'> 
-                        Nexibles.com reserves the right to modify or update this Return Policy at any time. Any changes will be effective upon posting on our website.
-                    </p>
-                    <p className='mb-1'>
-                        By making a purchase on Nexibles.com, you acknowledge and agree to the terms outlined in this Return Policy.
-                    </p>
+                    
                 </div>
             </div>
         </div>
     )
 }
 
-export default ReturnPolicy
+export default ReturnPolicy;

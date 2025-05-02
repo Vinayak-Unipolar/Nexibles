260fbae0d1b454498179dc7b630843d049c9d328
Gouthami200119 <gouthami@unipolar.in>
2025-05-02 14:49:12 +0530

    count price

--- src/components/home/NexiblesInstagramSection.jsx
+++ src/components/home/NexiblesInstagramSection.jsx
@@ -52,7 +52,8 @@ export default function NexiblesInstagramSection() {
       </div>
 
       {/* Instagram Feed - Changed to grid-cols-3 for all screen sizes */}
-      <div className="max-w-3xl mx-auto grid grid-cols-3 mb-12">
+      <div className="max-w-3xl mx-auto grid grid-cols-3 mb-12 gap-[0.2px]">
+
         {instaFeed.map((post, idx) => (
           <a
             key={idx}

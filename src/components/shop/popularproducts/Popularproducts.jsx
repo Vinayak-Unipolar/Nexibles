7a86b62faa9f4ec44f10788e890155d78fd6f183
Vinayakv01 <vinayakv949@gmail.com>
2025-06-11 13:41:51 +0530

    changesgategoryurl

--- src/components/shop/popularproducts/Popularproducts.jsx
+++ src/components/shop/popularproducts/Popularproducts.jsx
@@ -65,7 +65,7 @@ export default function PopularProducts() {
               <div
                 key={category.id}
                 className="flex flex-col items-center w-full transition-all duration-300 hover:shadow-lg rounded-lg p-4 cursor-pointer"
-                onClick={() => window.location.href = `/${category.cat_url}`}
+                onClick={() => window.location.href = `/category/${category.cat_url}`}
               >
                 <div className="relative w-full aspect-square flex items-center justify-center overflow-hidden">
                   <Image
@@ -87,7 +87,7 @@ export default function PopularProducts() {
                   <div className="flex justify-center space-x-2">
                     <div className="flex justify-center items-center gap-2 flex-wrap">
                       <Link
-                        href={`/${category.cat_url}`}
+                        href={`/category/${category.cat_url}`}
                         onClick={(e) => e.stopPropagation()}
                         className="px-4 py-1.5 text-xs font-medium rounded hover:bg-gray-100 text-black transition duration-300 whitespace-nowrap"
                       >
